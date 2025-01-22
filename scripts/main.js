6cf07f7cef6c3d65fc2cbe0fed36ec2a619268ab
Mahmud Rayhan <mahmudrayhan.j@gmail.com>
2025-01-23 04:30:06 +0600

    standard curriculum section done

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+
+function myFunction() {
+  var x = document.getElementById("responseNav-right");
+  if (x.className === "nav-right") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav-right";
+  }
+}
+
+function myFunction() {
+    var y = document.getElementById("responseNav-right");
+    if (y.className === "nav-center") {
+      y.className += " responsive";
+    } else {
+      y.className = "nav-center";
+    }
+  }
+
+  function myFunction() {
+    var y = document.getElementById("responseNav-right");
+    if (y.className === "nav-center") {
+      y.className += " responsive";
+    } else {
+      y.className = "nav-center";
+    }
+  }
